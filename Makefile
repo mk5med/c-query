56d30a5227db4c789419f4ef24f9db88cc9bc97e
local <>
2022-07-09 15:12:09 -0400

    Add string_extensions library

--- Makefile
+++ Makefile
@@ -2,16 +2,21 @@ CC=gcc
 CFLAGS=-I.
 DEPS = main.h\
 			attribute_lib/attribute.h\
-			attribute_lib/attribute_parse.h attribute_lib/attribute_print.h 
+			attribute_lib/attribute_parse.h attribute_lib/attribute_print.h\
+			string_extensions/string_extensions.h
 OBJ = obj/main.o\
 			obj/lib/printAttributeTree.o obj/lib/attribute.o\
-			obj/lib/createSubAttributeChain.o obj/lib/readToken.o
+			obj/lib/createSubAttributeChain.o obj/lib/readToken.o\
+			obj/lib/string_split.o
 ODIR = obj
 
 obj/%.o: %.c $(DEPS)
 	$(CC) -g -c -o $@ $< $(CFLAGS)
 
 obj/lib/%.o: attribute_lib/%.c $(DEPS)
+	$(CC) -g -c -o $@ $< $(CFLAGS)
+
+obj/lib/%.o: string_extensions/%.c $(DEPS)
 	$(CC) -g -c -o $@ $< $(CFLAGS)
 
 c-query: $(OBJ)
---
+++ string_extensions/string_split.c
@@ -0,0 +1,40 @@
+#include <string.h>
+#include <stdlib.h>
+#include "string_extensions.h"
+
+/**
+ * @brief Split a string `str` at the first occurance of a character in `target`.
+ *        The substring before the split is stored in `first` and substring after the split is stored in `remainder`.
+ *
+ * @param str Input string
+ * @param target Split delimiter
+ * @param first out KEY
+ * @param remainder out VALUE
+ */
+void str_split(char *str, char *target, char **first, char **remainder)
+{
+  // Duplicate the string
+  char *str_copy = strndup(str, strlen(str) + 1);
+
+  if (str_copy == NULL)
+    return; // Memory allocation failed
+
+  char *remainder_mem = str_copy; // Mutable pointer to the remainder
+  strsep(&remainder_mem, target); // Set '\0' at the first occurance of the delimiter
+
+  // Set first to the new shortened string
+  *first = strndup(str_copy, strlen(str_copy) + 1);
+
+  // remainder_mem is NULL when the target characters were not found
+  if (remainder_mem != NULL)
+  {
+    // Set remainder to the remainder of the shortned string
+    *remainder = strndup(remainder_mem, strlen(remainder_mem) + 1);
+  }
+  else
+  {
+    *remainder = NULL;
+  }
+
+  free(str_copy);
+}
